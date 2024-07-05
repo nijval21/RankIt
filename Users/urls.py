6c6e1e44338adf97d4bc7a1f52426fabee36912c
nijval21 <nijvalchaudhari043@gmail.com>
2024-07-05 18:10:59 +0530

    admin only view done for webscraping josaa website compeletely and storing in database

--- Users/urls.py
+++ Users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignupView, LoginView, NewsView, CRLAvailInstiView, CatAvailInstiView, TopBranchesYearWise, YearlyTrend, Branches
+from .views import SignupView, LoginView, NewsView, CRLAvailInstiView, CatAvailInstiView, TopBranchesYearWise, YearlyTrend, Branches, admin_only_view
 
 
 urlpatterns = [
@@ -11,4 +11,5 @@ urlpatterns = [
     path('topbranchesyearwise/', TopBranchesYearWise.as_view(), name='topbranchesyearwise'),
     path('yearlytrend/', YearlyTrend.as_view(), name='yearlytrend'),
     path('branches/', Branches.as_view(), name='branches'),
+    path('admin-view/', admin_only_view, name='admin-view'),
 ]
