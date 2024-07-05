77e0245322b4c90dc4a91b72712e305d598743d3
nijval21 <nijvalchaudhari043@gmail.com>
2024-07-05 18:08:58 +0530

    yearly trend view done

--- Users/urls.py
+++ Users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignupView, LoginView, NewsView, CRLAvailInstiView, CatAvailInstiView, TopBranchesYearWise
+from .views import SignupView, LoginView, NewsView, CRLAvailInstiView, CatAvailInstiView, TopBranchesYearWise, YearlyTrend
 
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('crlavailinsti/', CRLAvailInstiView.as_view(), name='crlavailinsti'),
     path('catavailinsti/', CatAvailInstiView.as_view(), name='catavailinsti'),
     path('topbranchesyearwise/', TopBranchesYearWise.as_view(), name='topbranchesyearwise'),
+    path('yearlytrend/', YearlyTrend.as_view(), name='yearlytrend'),
 ]
