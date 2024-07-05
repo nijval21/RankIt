a8986ce23d3f76dabe61faab1c87a5ae622dfac5
nijval21 <nijvalchaudhari043@gmail.com>
2024-07-05 18:04:01 +0530

    news view done

--- Users/views.py
+++ Users/views.py
@@ -7,6 +7,9 @@ from .models import User
 import jwt
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import JsonResponse
+import requests
+from bs4 import BeautifulSoup
 # Create your views here.
 
 
@@ -51,6 +54,47 @@ class LoginView(APIView):
             return Response({'error': 'Invalid password'}, status=status.HTTP_400_BAD_REQUEST)
         except User.DoesNotExist:
             return Response({'error': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+        
+class NewsView(APIView):
+    def get(self, request):
+        # user_id = get_user_from_token(request)
+        # email = request.data.get('email')
+        # password = request.data.get('password')
+        url = 'https://www.google.com/search?q=josaa&tbm=nws'
+
+    # Headers to mimic a real browser request
+        headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+        }
+
+        # Sending the HTTP request with headers
+        response = requests.get(url, headers=headers)
+
+        # Checking if the request was successful
+        if response.status_code == 200:
+        # Ensuring the correct encoding is used
+            response.encoding = response.apparent_encoding
+            
+            # Parsing the HTML content
+            soup = BeautifulSoup(response.text, 'html.parser')
+            
+            # Finding all news articles
+            articles = soup.find_all('div', class_='SoaBEf', limit=10)
+        
+        # List to store title and link of each article
+            top_articles = []
+            
+            # Extracting titles and links
+            for article in articles:
+                link = article.find('a', class_='WlydOe')['href']
+                title = article.find('div', class_='n0jPhd ynAwRc MBeuO nDgy9d').get_text()
+                top_articles.append({'title': title, 'link': link})
+        
+        # Return the top 10 news articles as JSON response
+            return JsonResponse({'articles': top_articles})
+        else:
+        # Return error response if failed to retrieve the webpage
+            return JsonResponse({'error': 'Failed to retrieve the webpage'}, status=500)
 
 def generate_jwt_token(user):
     payload = {
