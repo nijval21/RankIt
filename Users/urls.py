7d19c7a473b5b99bedd1fee307e4c5c4cacf8c8e
nijval21 <nijvalchaudhari043@gmail.com>
2024-07-05 18:05:20 +0530

    CRL available institutes view done

--- Users/urls.py
+++ Users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SignupView, LoginView, NewsView
+from .views import SignupView, LoginView, NewsView, CRLAvailInstiView
 
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('news/', NewsView.as_view(), name='news'),
+    path('crlavailinsti/', CRLAvailInstiView.as_view(), name='crlavailinsti'),
 ]
