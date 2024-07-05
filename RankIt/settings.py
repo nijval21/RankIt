4eb163bc92b29780ffef1c86e53c278cdc82c1fd
nijval21 <nijvalchaudhari043@gmail.com>
2024-07-05 18:23:21 +0530

    setting updated

--- RankIt/settings.py
+++ RankIt/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'users',
+    'Users',
 ]
 
 REST_FRAMEWORK = {
--- RankIt/urls.py
+++ RankIt/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
+    path('api/', include('Users.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
